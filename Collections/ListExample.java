268e68c444983ab569e249134460f9fd14392303
Patryk Olechno <panoptikum28@gmail.com>
2025-03-01 14:13:28 +0100

    Commit:Collections:List2

--- Collections/ListExample.java
+++ Collections/ListExample.java
@@ -1,13 +1,24 @@
 package Collections;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListExample {
 
     public static void main(String[] args) {
-        List<String> list = List.of("Tarczyn, Warszawa, Polska", "Wrocław, Polska", "Kraków, Polska");
-        for (String s : list) {
-            System.out.println(s);
+//        List<String> list = List.of("Tarczyn, Warszawa, Polska", "Wrocław, Polska", "Kraków, Polska");
+//        for (String s : list) {
+//            System.out.println(s);
+
+        List<String> stringList = new ArrayList<>();
+        stringList.add("Budzik");
+        stringList.add("Tomasz");
+        stringList.add("Tatar");
+        stringList.add("Dąb");
+
+        System.out.println(stringList.size());
         }
+
+
     }
-}
+
